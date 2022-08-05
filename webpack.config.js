c2a2e7a0b08d020c91e835a5cbc13c087f610e30
tue ho <super-Dev>
2022-08-05 21:22:56 +0700

    oke

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js',
+  },
+  target: 'node',
+  externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  mode: process.env.NODE_ENV,
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    fallback: {
+      fs: false,
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$|jsx/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+}
